334c69dd7e91ac7762bf395b7431dcc8900aadaa
tdd <tang2dan@gmail.com>
2018-03-15 13:40:14 +0800

    add example

--- ni/back/lib/src/main/main.go
+++ ni/back/lib/src/main/main.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"images"
+	"images/color"
+	"images/gif"
+	"io"
+	"math"
+	"math/rand"
 )
 
 func main(){
@@ -32,4 +39,54 @@ func main(){
 	fmt.Println("gopl.io/ch1/echo3 ============= ")
 	fmt.Println(strings.Join(os.Args[1:]," "))
 	fmt.Println(os.Args[1:])
+
+	//**** gopl.io/ch1/lissajous
+	// Lissajous generates GIF animations of random Lissajous figures
+	/**
+	import {
+		"images"
+		"images/color"
+		"images/gif"
+		"io"
+		"math"
+		"math/rand"
+		"os"
+	}
+	**/
+
+	var palette = []color.Color{color.White,color.Black}
+	const(
+		whiteIndex = 0 //first color in palette
+		blackIndex = 1 //next color in palette
+	)
+
+	func main(){
+		lissajous(os.Stdout)
+	}
+
+	func lissajous(out io.Writer){
+		const(
+			cysles = 5 //number of complete x oscillator revolutions
+			res = 0.001 //angular resolution
+			size = 100 // image canvas cover [-size..+size]
+			nframes = 64 // number of animation frames
+			delay = 8 //delay between frame in 10ms units
+		)
+		freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+		anim := gif.GIF{LoopCount:nframes}
+		phase := 0.0 //phase difference
+		for i:=0;i<nframes; i++{
+			rect := image.Rect(0,0,2*size+1,2*size+1)
+			img := image.NewPaletted(rect,palette)
+			for t:=0.0; t < cycles*2*math.Pi;t += res{
+				x := math.Sin(t)
+				y := math.Sin(t*freq + phase)
+				img.SetColorIndex(size + int(x*size+0.5),size+int(y*size+0.5),blackIndex)
+			}
+			phase += 0.1
+			anim.Delay = append(anim.Delay,delay)
+			anim.Image = append(anim.Image,img)
+		}
+		gif.EncodeAll(out, &anim) //NOTE: ignoring encoding errors
+	}
 }
