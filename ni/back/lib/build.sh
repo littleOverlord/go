905437fdecaa2b24bf95d3a3212b5880fdb52daa
tdd <tdd_style@163.com>
2018-05-24 23:27:20 +0800

    change the development directory

--- ni/back/lib/build.sh
+++
@@ -1,6 +0,0 @@
-set GOPATH=%GOPATH%;%CD%
-go build hello
-go install hello
-go build main
-go install main
-pause
--- ni/boot/start.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH./libadatper:./libv8c"
-erl +sbt ns +sub true -boot start_sasl -config sasl -setcookie abc -name yszz@127.0.0.1 -env ERL_LIBS ../lib +pc unicode  -detached
