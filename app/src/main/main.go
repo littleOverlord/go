acb55cd3bf510de6d642c30d67f6d6e86e70d3ed
tdd <tdd_style@163.com>
2018-06-07 08:41:47 +0800

    add test ^ & | << >>

--- app/src/main/main.go
+++ app/src/main/main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	// "net/http"
 	
-	"router"
+	// "router"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		err := router.Distribute(w,r)
-		if(err != nil){
+	var a int8 = ^120
+	fmt.Printf("%08b\n",a)
+
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+// 		err := router.Distribute(w,r)
+// 		if(err != nil){
 			
-			fmt.Println(r.URL.Path,err.Error())
-		}
-   });
-	http.ListenAndServe(":1224",nil)
+// 			fmt.Println(r.URL.Path,err.Error())
+// 		}
+//    });
+// 	http.ListenAndServe(":1224",nil)
 }
