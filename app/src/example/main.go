b605c66f6fa9285111401a121ec586886c892de0
唐丹丹 <tangdandan@tangdandandeMacBook-Pro.local>
2018-10-22 07:04:55 +0800

    add 4.4.3 example

--- app/src/example/main.go
+++ app/src/example/main.go
@@ -9,4 +9,16 @@ func main()  {
 	fmt.Printf("The operating system is: %s\n", goos)
 	path := os.Getenv("GOPATH")
 	fmt.Printf("goPath is %s\n", path)
+	//4.4.3
+	console()
+	//4.4.4 TODO..
+}
+
+// console
+func console(){
+	//func Printf(format string, list of variables to be printed)
+	//%s string; %v default type change;
+	fmt.Printf("test Printf with a string: %s\n","like this!!")// test Printf with a string: like this!!
+	s := fmt.Sprintf("test Sprintf with a string: %s\n","like this!") //
+	fmt.Print("print s == ",s);// print s == test Sprintf with a string: like this!
 }
