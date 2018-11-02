3ce6155bb374c111fc0f2b0635fd308c1b03f114
tdd <tdd_style@163.com>
2018-11-03 00:15:35 +0800

    modify proxy

--- ni/src/proxy/main.go
+++ ni/src/proxy/main.go
@@ -4,7 +4,7 @@ package main
 import (
     "bytes"
     "fmt"
-    "io"
+    // "io"
     "log"
     "net"
     "net/url"
@@ -13,8 +13,9 @@ import (
 
 func main() {
     log.SetFlags(log.Ltime | log.Lshortfile)
+    log.Println()
     //监听1314端口
-    s, err := net.Listen("tcp", ":80")
+    s, err := net.Listen("tcp", ":8080")
     if err != nil {
         log.Panic(err)
     }
@@ -24,12 +25,14 @@ func main() {
         if err != nil {
             log.Panic(err)
         }
+        fmt.Printf("%d======\n",1)
         go proxy(c)
     }
 }
 func proxy(client net.Conn) {
     defer client.Close()
 
+    fmt.Printf("localAddr = %s; RemoteAddr = %s \n",client.LocalAddr(), client.RemoteAddr())
     var b [1024]byte
     n, err := client.Read(b[:])
     if err != nil {
@@ -57,19 +60,21 @@ func proxy(client net.Conn) {
             addr = u.Path
         }
     }
-    fmt.Printf("address = %s\n",addr)
+    fmt.Printf("n = %d\n",n)
+    fmt.Printf("Opaque = %s, Scheme = %s, Opaque = %s, Host = %s, Path = %s \n", u.Opaque, u.Scheme, u.Opaque, u.Host, u.Path)
+    fmt.Printf("addr = %s\n",addr)
     //连接远程服务器
-    s, err := net.Dial("tcp", addr)
-    if err != nil {
-        log.Println(err)
-        return
-    }
-    if methon == "CONNECT" {
-        fmt.Fprint(client, "HTTP/1.1 200 Connection established\r\nConnection: close\r\n\r\n")
-    } else {
-        s.Write(b[:n])
-    }
+    // s, err := net.Dial("tcp", addr)
+    // if err != nil {
+    //     log.Println(err)
+    //     return
+    // }
+    // if methon == "CONNECT" {
+    //     fmt.Fprint(client, "HTTP/1.1 200 Connection established\r\nConnection: close\r\n\r\n")
+    // } else {
+    //     s.Write(b[:n])
+    // }
     //进行转发
-    go io.Copy(s, client)
-    io.Copy(client, s)
+    // go io.Copy(s, client)
+    // io.Copy(client, s)
 }
