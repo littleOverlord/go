575f56ab04d3ba87001620335f17a687a1e1e233
tdd <tdd_style@163.com>
2018-11-07 18:51:47 +0800

    add example

--- app/src/example/way_4/for.go
+++ app/src/example/way_4/for.go
@@ -62,6 +62,20 @@ func for_test(){
 		i, j, s = i + 1, j + 1, s + "a"{
 			fmt.Println("Value of i , j, s : ",i ,j ,s)
 	}
+
+	i := 0
+	for { //since there are no checks, this is an infinite loop
+		if i >= 3 { break }
+		//break out of this for loop when this condition is met
+		fmt.Println("Value of i is:", i)
+		i++
+	}
+	fmt.Println("A statement just after for loop.")
+	
+	for i := 0; i<7 ; i++ {
+		if i%2 == 0 { continue }
+		fmt.Println("Odd:", i)
+	}
 }
 
 /**
