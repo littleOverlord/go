021650779c1b66ff2eee87225afa555fa4c60dc4
tdd <tdd_style@163.com>
2018-11-26 17:27:20 +0800

    modify effect

--- app/src/trhc/static/js/common.js
+++ app/src/trhc/static/js/common.js
@@ -1,7 +1,7 @@
 var modules = {};
 (function(){
     var exports = modules["common"] = {};
-    var nav,con,nowNav,currClass,clientWidth,clientHeight;
+    var nav,con,nowNav,currClass,clientWidth,clientHeight,timer;
     var ElsByAttr = function(keys){
         var r = {};
         for(var i = 0,len = keys.length; i < len; i++){
@@ -22,25 +22,27 @@ var modules = {};
             (function(key){
                 nav[key].node.click(function(){
                     setNow(key);
+                    $('html,body').animate({scrollTop:con[key].offest.top}, 800);
                 })
             })(k);
         }
         nav[now].node.addClass(currClass);
+        nowNav = now;
     };
     var setNow = function(key){
         if(key != nowNav){
             nav[key].node.addClass(currClass);
             nav[nowNav].node.removeClass(currClass);
             nowNav = key;
-            console.log(con[now].offest.top);
-            $('html,body').animate({scrollTop:con[now].offest.top}, 800);
+            console.log(con[nowNav].offest.top);
+            
         }
     };
     var posResize = function(list){
         for(var i in list){
             list[i].offest = list[i].node.offset();
             list[i].size = {
-                w : list[i].node.with(),
+                w : list[i].node.width(),
                 h : list[i].node.height()
             }
         }
@@ -50,8 +52,8 @@ var modules = {};
             w = document.body.clientWidth || document.documentElement.clientWidth,
             h = document.body.clientHeight || document.documentElement.clientHeight,
             s = w/1000;
-            clientWidth = w;
-            clientHeight = h;
+            clientWidth = $(window).width();
+            clientHeight = $(window).height();
         if(s<1){
             s = s<1 && s >0.6?s:0.6;
             body.attr("style","font-size:"+s*20+"px;");
@@ -61,10 +63,18 @@ var modules = {};
         posResize(con);
     };
     var windowScroll = function(e){
-        var t = $(window).scrollTop();
+        var t = $(window).scrollTop(),
+            l = t + clientHeight/2;
         for(let k in con){
-            if(t >= con[k].offest.top && t < con[k].offest.top+con[k].size.h){
-                return setNow(k);
+            if( l >= con[k].offest.top && l <= con[k].offest.top+con[k].size.h){
+                clearTimeout(timer);
+                timer = setTimeout((function(key){
+                    return function(){
+                        clearTimeout(timer);
+                        setNow(key);
+                    }
+                })(k),100);
+                return;
             }
         }
         console.log(e);
