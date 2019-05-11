9a88a7e018fa15929cc9b1f3e3bf2392c0f88f5a
tdd <tdd_style@163.com>
2019-05-11 17:22:07 +0800

    add server mod && tls file

---
+++ src/ni/server/server_test.go
@@ -0,0 +1,36 @@
+// Copyright 2019 tdd authors
+package server
+
+import (
+	"fmt"
+	"testing"
+	"path"
+)
+
+func TestGetWorkSpace(t *testing.T){
+	if WorkSpace == "" || WorkSpace == "." {
+		t.Error(fmt.Sprintf(`WorkSpace can't be "%s"`,WorkSpace))
+	}
+}
+func TestIsDir(t *testing.T){
+	logPath := path.Join("back","log")
+	filePath := path.Join(WorkSpace,logPath)
+	if !IsDir(filePath) {
+		t.Error(fmt.Sprintf(`"%s" should be a dir!`,filePath))
+	}
+	filePath = path.Join(filePath,"mongodb.log")
+	if IsDir(filePath) {
+		t.Error(fmt.Sprintf(`"%s" should be a file!`,filePath))
+	}
+}
+func TestIsFile(t *testing.T){
+	logPath := path.Join("back","log")
+	filePath := path.Join(WorkSpace,logPath)
+	if IsFile(filePath) {
+		t.Error(fmt.Sprintf(`"%s" should be a dir!`,filePath))
+	}
+	filePath = path.Join(filePath,"mongodb.log")
+	if !IsFile(filePath) {
+		t.Error(fmt.Sprintf(`"%s" should be a file!`,filePath))
+	}
+}
