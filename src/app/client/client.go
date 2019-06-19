adda9ebd8f512223042915277dd3610b70636003
tdd <tdd_style@163.com>
2019-06-19 20:54:35 +0800

    add client mod

--- src/app/client/client.go
+++ src/app/client/client.go
@@ -1,7 +1,10 @@
 package client
 
 import (
+	"fmt"
 	"ni/mongodb"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 var uid int
@@ -14,4 +17,20 @@ func init() {
 
 func initUid() {
 	col, ctx, cancel := mongodb.Collection("client")
+	filter := bson.M{}
+	defer cal()
+	// filter posts tagged as golang
+	filter := bson.M{key:"uid"}
+	{$set:{key:"uid"}, $inc : { "value" : 1 }}
+	cursor := col.FindOneAndUpdate(ctx, filter)
+
+	// iterate through all documents
+	// for cursor.Next(ctx) {
+	var p Post
+	// decode the document
+	if err := cursor.Decode(&p); err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Printf("post: %+v\n", p)
+
 }
