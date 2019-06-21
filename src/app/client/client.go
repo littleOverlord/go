5a820df81e6a2f1c4222113dc702015e8355cf9b
tdd <tdd_style@163.com>
2019-06-21 20:01:08 +0800

    test client

--- src/app/client/client.go
+++ src/app/client/client.go
@@ -2,44 +2,87 @@ package client
 
 import (
 	"fmt"
+	"ni/logger"
 	"ni/mongodb"
 	"sync"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-var uid = &struct {
-	key   string
-	value int
-}{
-	key:   "uid",
-	value: 0,
-}
 var mux sync.Mutex
 
+type Uid struct {
+	_id   string
+	key   string
+	value int
+}
+
 func init() {
-	initUid()
+	initUID()
 	// regist ws(s) handlers
 	port()
 }
 
-func initUid() {
+func initUID() {
 	col, ctx, cancel := mongodb.Collection("client")
-	defer cancel()
+	uid := &Uid{
+		key:   "uid",
+		value: 0,
+	}
+	defer func() {
+		cancel()
+		if p := recover(); p != nil {
+			fmt.Println(p)
+			logger.Error(p.(string))
+		}
+	}()
 	// filter posts tagged as golang
 	filter := bson.M{"key": "uid"}
-	opts := options.FindOneAndUpdate()
-	opts.SetUpsert(true)
-	opts.SetReturnDocument(options.After)
 
-	cursor := col.FindOneAndUpdate(ctx, filter, bson.M{"$set": bson.M{"key": "uid"}, "$inc": bson.M{"value": 1}}, opts)
+	cursor := col.FindOne(ctx, filter)
 
 	// iterate through all documents
 	// for cursor.Next(ctx) {
 	// decode the document
 	if err := cursor.Decode(uid); err != nil {
-		fmt.Println(err.Error())
+		panic(err.Error())
 	}
 	fmt.Println(uid)
+	if uid.value == 0 {
+		_, err := col.InsertOne(ctx, bson.M{"key": "uid", "value": 10000})
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+}
+
+//GetUID is the way to get new user id
+func GetUID() (int, error) {
+	var (
+		uid *Uid
+		err error
+	)
+	col, ctx, cancel := mongodb.Collection("client")
+	defer cancel()
+	cursor := col.FindOne(ctx, bson.M{"key": "uid"})
+	if err = cursor.Decode(uid); err != nil {
+		return 0, err
+	}
+
+	id, err := primitive.ObjectIDFromHex(uid._id)
+	if err != nil {
+		return 0, err
+	}
+
+	// set filters and updates
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"value": uid.value + 1}}
+
+	// update document
+	_, err = col.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return 0, err
+	}
+	return uid.value, nil
 }
--- src/app/client/client_test.go
+++ src/app/client/client_test.go
@@ -1,9 +1,38 @@
 package client
 
-import "testing"
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
 
 func TestInitUid(t *testing.T) {
-	if !(uid.value > 0) {
+	uid, err := GetUID()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if uid != 10000 {
 		t.Error("uid init fail!")
 	}
+}
+
+func TestGoGetUid(t *testing.T) {
+	var (
+		wg sync.WaitGroup
+		tm time.Time = time.Now()
+	)
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			wg.Done()
+			uid, err := GetUID()
+			if err != nil {
+				t.Error(err.Error())
+			}
+			fmt.Println(uid)
+		}()
+	}
+	wg.Wait()
+	time.Since(tm)
 }
